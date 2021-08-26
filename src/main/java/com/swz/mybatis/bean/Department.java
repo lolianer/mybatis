4404ebb5fb1a26fd33af7448ba42a651366716f4
shen_wzhong <1172595124@qq.com>
2021-08-26 18:48:28 +0800

    first

---
+++ src/main/java/com/swz/mybatis/bean/Department.java
@@ -0,0 +1,53 @@
+package com.swz.mybatis.bean;
+
+import java.util.List;
+
+/**
+ * @author shen_wzhong
+ * @create 2021-08-26 15:41
+ */
+public class Department {
+    private Integer id;
+    private String departmentName;
+    private List<Employee> emps;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String departmentName) {
+        this.id = id;
+        this.departmentName = departmentName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public List<Employee> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(List<Employee> emps) {
+        this.emps = emps;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+}
